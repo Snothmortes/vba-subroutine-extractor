40c9a6893093d663234cff9405e37a33f69b408c
Glen <72626315+Snothmortes@users.noreply.github.com>
2024-01-08 18:26:48 +0100

    Changes committed on 2024-01-08 18:26:48

--- Deploy.ps1
+++ Deploy.ps1
@@ -10,9 +10,7 @@ vsce package
 
 # Step 3: Install the extension globally in VSCode
 # Replace with your extension ID if needed
-$extensionId = "vba-subroutine-extractor"
 code --install-extension $vsixFilePath
-code --enable-extension $extensionId
 
 # Optional: Show a success message
 Write-Host "Deployment complete. Extension installed and enabled globally."
